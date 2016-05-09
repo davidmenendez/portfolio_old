774206d5810b05efbf97e988af0180d5bb116faf
David Menendez <damenend@gmail.com>
2016-05-09 14:00:29 -0500

    added routes and layout for entries

--- routes/index.js
+++ routes/index.js
@@ -13,31 +13,31 @@ router.get('/about', function(req, res, next) {
 
 //Portfolio Entries
 router.get('/portfolio/hadassah', function(req, res, next) {
-  res.render('portfolio/hadassah', { title: 'About' });
+  res.render('portfolio/hadassah', { title: 'Hadassah' });
 });
 router.get('/portfolio/colorado-childrens-hospital', function(req, res, next) {
-  res.render('portfolio/colorado-childrens-hospital', { title: 'About' });
+  res.render('portfolio/colorado-childrens-hospital', { title: 'Colorado Childrens Hospital' });
 });
 router.get('/portfolio/blackbaud', function(req, res, next) {
-  res.render('portfolio/blackbaud', { title: 'About' });
+  res.render('portfolio/blackbaud', { title: 'Blackbaud' });
 });
 router.get('/portfolio/be-a-character', function(req, res, next) {
-  res.render('portfolio/be-a-character', { title: 'About' });
+  res.render('portfolio/be-a-character', { title: 'Be-A-Character' });
 });
 router.get('/portfolio/bridgewater', function(req, res, next) {
-  res.render('portfolio/bridgewater', { title: 'About' });
+  res.render('portfolio/bridgewater', { title: 'Bridgewater' });
 });
 router.get('/portfolio/national-hamburger-month', function(req, res, next) {
-  res.render('portfolio/national-hamburger-month', { title: 'About' });
+  res.render('portfolio/national-hamburger-month', { title: 'National Hamburger Month' });
 });
 router.get('/portfolio/zimmerman-blog', function(req, res, next) {
-  res.render('portfolio/zimmerman-blog', { title: 'About' });
+  res.render('portfolio/zimmerman-blog', { title: 'Zimmerman Blog' });
 });
 router.get('/portfolio/bikestreet', function(req, res, next) {
-  res.render('portfolio/bikestreet', { title: 'About' });
+  res.render('portfolio/bikestreet', { title: 'Bikestreet' });
 });
 router.get('/portfolio/hottest-halloween', function(req, res, next) {
-  res.render('portfolio/hottest-halloween', { title: 'About' });
+  res.render('portfolio/hottest-halloween', { title: 'Hottest Halloween' });
 });
 
 module.exports = router;
